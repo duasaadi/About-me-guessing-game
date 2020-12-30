63b29d5816fca8c91128030ad430aafb82897962
duasaadi <doooshy@hotmail.com>
2020-12-30 13:51:20 +0200

    changes

--- Js/script.js
+++ Js/script.js
@@ -13,49 +13,70 @@ printName(userName);
 
 alert('welcome to my quiz :)');
 
-// var favPlane = prompt("Guess what is my favorite type of fighter planes?");
-
+var favPlane = prompt("Guess what is my favorite type of fighter planes?");
+var counter = 0;
 // console.log(favPlane.toLowerCase());
 // console.log(favPlane.toUpperCase());
 // console.log(favPlane.toLowerCase() === ('mig-29' || 'mig '))
 
-// if (favPlane.toLowerCase() === 'mig29' || favPlane.toLowerCase() === 'mig') {
+if (favPlane.toLowerCase() === 'mig29' || favPlane.toLowerCase() === 'mig') {
 
-//     alert('you are right, I still like its high aerodynamic specifications:)');
+    alert('you are right, I still like its high aerodynamic specifications:)');
+    counter++;
 
-// } else {
+} else {
 
-//     alert('unfortunatlly, you did not guess :(');
-// };
-
-// var feeling= confirm('do you think that I am afraid of flying ?(yes/ No)');
-
-// var feeling= ('do you think that I am afraid of flying ?(yes/ No)');
-// console.log(feeling);
-// if(feeling.toLowerCase()==='yse'){ 
-//     alert(' yes, Great! :)');
-// } else {
-//     alert('No , I like it!');
-// }
-
-
-// var gravity = prompt('Do you think that i like the feeling of lacking gravity in Rollercoaster? ') ;
-
-//     console.log(gravity);
-// if(gravity.toLowerCase()==='yse'){ 
-//     alert(' yes, Great! :)');
-// } else {
-//     alert('No , I like it!');
-// }
+    alert('unfortunatlly, you did not guess :(');
+};
 
 
 
-// // var flyPeriod = prompt("Guess How long could you fly with Mig-29 by paying 15000$ ? ");
+var feeling = prompt('do you think that I am afraid of flying ?(yes/ No)');
+console.log(feeling);
+if (feeling.toLowerCase() === 'yse') {
+
+    alert(' yes, Great! :)');
+    counter++;
+
+} else {
+    alert('No , I like it!');
+}
 
 
 
+var gravity = prompt('Do you think that i like the feeling of lacking gravity in Rollercoaster? ');
+
+console.log(gravity);
+if (gravity.toLowerCase() === 'yse') {
+    alert(' yes, Great! :)');
+    counter++;
+} else {
+    alert('No , I like it!');
+}
 
 
+var flyPeriod = prompt("Guess How long could you fly with Mig-29 by paying 15000$ ? ");
+
+console.log(flyPeriod);
+if (flyPeriod === '1 hour') {
+    alert(' yes, Great! :)');
+    counter++;
+} else {
+    alert('No ');
+
+
+}
+
+
+var BA = prompt('Do you think that i studied Aeronautics? ') ;
+
+    console.log(BA);
+if(BA.toLowerCase()==='yse'){ 
+    alert(' yes, Great! :)');
+    counter++ ;
+}else {
+    alert('No');
+}
 
 
 
@@ -63,51 +84,40 @@ alert('welcome to my quiz :)');
 
 // // sixth question
 
-// var num= 7;
-// var i=0; 
-//  while(i<4 && num !== "7" ){
+var num = 7;
+var i = 0;
+while (i < 4 && num !== "7") {
 
-//      num= prompt ("Guess my favorite number from 1 to 20? (you have 4 opportunities to guess)");
-//      i++;
+    num = prompt("Guess what is my favorite number from 1 to 20? (you have 4 opportunities to guess)");
+    i++;
 
-//     if (num >7){
-//         alert('it is higher than mine');
-//     }else {
-//         alert ('it is lower than mine');
-//     } 
+    if (num > 7) {
+        alert('it is higher than mine');
+    } else if (num = 7) {
+        alert('it is true');
+        counter++;
+        break;
+    } else {
+        alert('it is lower than mine');
+    }
 
-// }
-
+}
 
 
 //7th question
 
+var check = ['Okra', 'Fettuccine Alfredo', 'Stake', 'Pizza', 'Stuffed grape leaves'];
 
-// var numOfTrying = 0;
-// // var lunchArray= ['Okra' , 'Fettuccine Alfredo', 'Stake' , 'Pizza' , 'Stuffed grape leaves'];
-
-// var Q7 = prompt('what kind of food that I prefer at lunch ?');
-
-// while (numOfTrying < 6 && Q7.toLowerCase() !== 'okra' || Q7.toLowerCase() !== 'fettuccine alfredo' || Q7.toLowerCase() !== 'stake') {
-
-//     prompt('what kind of food that I prefer at lunch ?');
-//     i++;
-// }
-
-
-// // console.log ([lunchArray]);
-
-// for (var j=0 ; j<lunchArray.length ; j++){
-
-//     alert (lunchArray[j]);
-// }
 
 var numOfTrying = 0;
-var check = false ;
-do  {
+var check = false;
+do {
     var Q7 = prompt('what kind of food that I prefer at lunch ?');
-    for(var i =0 ; check )
+    for (var i = 0; i < check; i++);
 
-    numOfTrying++ ;
+    numOfTrying++;
+    alert('ya, I like it very much^ ^')
+    counter++;
+    break;
 
-}while ( numOfTrying < 6 && check!==false)
+} while (numOfTrying < 6 && check !== false)
