91802b6d82bd02759fb7b73625cba1b2b45e088c
duasaadi <doooshy@hotmail.com>
2021-01-16 14:06:35 +0200

    lab 5

--- Js/script.js
+++ Js/script.js
@@ -15,13 +15,13 @@ alert('welcome to my quiz :)');
 
 var favPlane = prompt("Guess what is my favorite type of fighter planes?");
 var counter = 0;
-// console.log(favPlane.toLowerCase());
-// console.log(favPlane.toUpperCase());
 // console.log(favPlane.toLowerCase() === ('mig-29' || 'mig '))
 
 if (favPlane.toLowerCase() === 'mig29' || favPlane.toLowerCase() === 'mig') {
 
     alert('you are right, I still like its high aerodynamic specifications:)');
+    // console.log(favPlane.toLowerCase());
+
     counter++;
 
 } else {
@@ -32,23 +32,25 @@ if (favPlane.toLowerCase() === 'mig29' || favPlane.toLowerCase() === 'mig') {
 
 
 var feeling = prompt('do you think that I am afraid of flying ?(yes/ No)');
-console.log(feeling);
 if (feeling.toLowerCase() === 'yse') {
 
-    alert(' yes, Great! :)');
+    alert(' No , I like it:)');
+    console.log(feeling);
+
     counter++;
 
 } else {
-    alert('No , I like it!');
+    alert('Great!');
 }
 
 
 
 var gravity = prompt('Do you think that i like the feeling of lacking gravity in Rollercoaster? ');
 
-console.log(gravity);
-if (gravity.toLowerCase() === 'yse') {
+if (gravity.toLowerCase() === 'yes') {
     alert(' yes, Great! :)');
+    console.log(gravity);
+
     counter++;
 } else {
     alert('No , I like it!');
@@ -57,29 +59,29 @@ if (gravity.toLowerCase() === 'yse') {
 
 var flyPeriod = prompt("Guess How long could you fly with Mig-29 by paying 15000$ ? ");
 
-console.log(flyPeriod);
 if (flyPeriod === '1 hour') {
     alert(' yes, Great! :)');
+    console.log(flyPeriod);
+
     counter++;
+    break ;
 } else {
     alert('No ');
 
 
 }
 
-
 var BA = prompt('Do you think that i studied Aeronautics? ') ;
 
-    console.log(BA);
-if(BA.toLowerCase()==='yse'){ 
+if(BA.toLowerCase()==='yes'){ 
     alert(' yes, Great! :)');
+    console.log(BA);
+
     counter++ ;
+    break;
 }else {
     alert('No');
 }
-
-
-
 
 
 // // sixth question
@@ -93,6 +95,7 @@ while (i < 4 && num !== "7") {
 
     if (num > 7) {
         alert('it is higher than mine');
+        console.log(num);
     } else if (num = 7) {
         alert('it is true');
         counter++;
@@ -106,18 +109,34 @@ while (i < 4 && num !== "7") {
 
 //7th question
 
-var check = ['Okra', 'Fettuccine Alfredo', 'Stake', 'Pizza', 'Stuffed grape leaves'];
+var lunchArray = ['Okra', 'Fettuccine Alfredo', 'Stake', 'Pizza', 'Stuffed grape leaves'];
 
 
 var numOfTrying = 0;
 var check = false;
 do {
     var Q7 = prompt('what kind of food that I prefer at lunch ?');
-    for (var i = 0; i < check; i++);
+    for (var i = 0; i < lunchArray; i++){
 
     numOfTrying++;
-    alert('ya, I like it very much^ ^')
     counter++;
-    break;
+
+    break;}
+
+    if (check !== false){
+        alert ("yes.Great ");
+        console.log(lunchArray[i]);
+    }else {
+        
+        alert('even it is not in my list i think i like it');
+     }
 
 } while (numOfTrying < 6 && check !== false)
+
+
+
+
+alert("Good Job!... You got " + counter + " lunchArray")
+// console.log("Your final score is " + userScore");
+alert("Thank you for your time " + userName + ".");
+// console.log("Thank you for your time " + userName + ".");
